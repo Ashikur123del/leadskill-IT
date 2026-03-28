51bcd4ff2fdb0cf82d40fa769033731c6032e3e9
ashik <leadskillit@gmail.com>
2026-03-28 15:50:39 +0600

    update

--- src/Components/CodeConductComponent/CodeConduct.jsx
+++ src/Components/CodeConductComponent/CodeConduct.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
 
 const CodeConduct = () => {
   
   return (
     <section className="py-10 md:py-20 px-4 bg-gray-50">
-      <div className="max-w-5xl mx-auto">
+      <div className="max-w-6xl mx-auto">
         <h2 className="text-4xl md:text-5xl font-bold text-center text-[#1e5631] mb-2">Code of Conduct</h2>
         <div className="h-[5px] w-50 md:w-100 mx-auto commonbgcolor rounded-2xl mb-10"></div>
-        <div className="">
-            <img src="./G-5.webp" alt="" />
+        <div className="w-full">
+            <img src="22222.png" alt="" className='w-full' />
       </div>
       </div>
     </section>
@@ -16,3 +15,5 @@ const CodeConduct = () => {
 };
 
 export default CodeConduct;
+
+      
--- src/Pages/CodeOfConduct.jsx
+++ src/Pages/CodeOfConduct.jsx
@@ -5,7 +5,7 @@ import HeroComn from '../Components/Sherd-compo/HeroComn'
 const CodeOfConduct = () => {
   return (
     <div>
-        <HeroComn   imageSrc = "/eduman-5.webp" height = "h-64 md:h-96 lg:h-full"/>
+        <HeroComn   imageSrc = "/11111.png" height = "h-64 md:h-96 lg:h-full"/>
         <CodeConduct />
     </div>
   )
